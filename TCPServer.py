d379c35f4087208a16f5f5b25240f0953266bf40
Noah Stelting <noahstelting@gmail.com>
2022-11-19 18:07:10 -0800

    HTTP Respones codes 200, 400, and 404 are done. Still need 308 and 408

--- TCPServer.py
+++ TCPServer.py
@@ -8,9 +8,8 @@ serverPort = 8080
 # Create TCP welcoming socket
 serverSocket = socket(AF_INET,SOCK_STREAM)
 
-#serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-
 # Bind the server port to the socket
+# NOTE: I have to put in my IP to get it to work on my device. If I just put '' it onyl works on other devices on my network.
 serverSocket.bind(("" ,serverPort))
 
 # Server begins listerning foor incoming TCP connections
@@ -24,32 +23,29 @@ while True: # Loop forever
 
     # Read from socket (but not address as in UDP)
     page = connectionSocket.recv(1024).decode()
-    filename = page.split(" ")[1]
-
+    print(page)
+    
     #for testing purposes
     #filename = "file.txt"
 
-    #checking for the existence of the file
-    '''if(not str(path.exists(filename))):
-        connectionSocket.send(b'HTTP/1.0 404 Not Found\r\n\r\n')
-        # Close connection as the file does not exist
-        connectionSocket.close()'''
-    
     try:
-        file = open("." + filename)
-        bytes = file.read()
-        file.close()
+        filename = "." + page.split(" ")[1]
 
-        #Send one HTTP header line into socket
-        connectionSocket.send(b'HTTP/1.0 200 OK\r\n\r\n')
-        #Send the content of the requested file to the client
-        for i in range(0, len(bytes)):
-            connectionSocket.send(bytes[i].encode())
+        #Checking for the existence of the file
+        if(not path.exists(filename)):
+            connectionSocket.send(b'HTTP/1.0 404 Not Found\r\n\r\n')
+        else: 
+            file = open(filename)
+            bytes = file.read()
+            file.close()
 
-        # Close connectiion too client (but not welcoming socket)
-        connectionSocket.close()
+            #Send one HTTP header line into socket
+            connectionSocket.send(b'HTTP/1.0 200 OK\r\n\r\n')
+            #Send the content of the requested file to the client
+            for i in range(0, len(bytes)):
+                connectionSocket.send(bytes[i].encode())
     except:
-        connectionSocket.send(b'HTTP/1.0 400 BAD\r\n\r\n')
-
-        # Close connectiion too client (but not welcoming socket)
+        connectionSocket.send(b'HTTP/1.0 400 Bad request\r\n\r\n')
+    finally:
+        # Close connection to client (but not welcoming socket)
         connectionSocket.close()
