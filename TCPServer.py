63231ddea3afc04c336dace212de2e92f9d28e96
Noah Stelting <noahstelting@gmail.com>
2022-11-22 21:04:23 -0800

    Add code for 408 response.

--- TCPServer.py
+++ TCPServer.py
@@ -3,7 +3,7 @@ import os
 from socket import *
 from os import path
 from datetime import datetime
-
+import time
 
 # Specify Server Port
 serverPort = 8080
@@ -19,14 +19,6 @@ serverSocket.bind(("" ,serverPort))
 serverSocket.listen(1)
 print ('The server is ready to receive')
 
-# Server waits on accept for incoming requests.
-# New socket created on return
-connectionSocket, addr = serverSocket.accept()
-
-# Read from socket (but not address as in UDP)
-page = connectionSocket.recv(1024).decode()
-filename = "." + page.split(" ")[1]
-
 while True: # Loop forever
     # Server waits on accept for incoming requests.
     # New socket created on return
@@ -35,7 +27,9 @@ while True: # Loop forever
     # Read from socket (but not address as in UDP)
     page = connectionSocket.recv(1024).decode()
     print(page)
-    
+
+    #Get the current time
+    currentTime = datetime.now()
 
     try:
         filename = "." + page.split(" ")[1]
@@ -47,27 +41,35 @@ while True: # Loop forever
             #Check if the file has been modified.
             lastModifiedTime = os.path.getmtime(filename)
             time = datetime.fromtimestamp(lastModifiedTime)
-            #Get the current time
-            currentTime = datetime.now()
-            #If the difference between currentTime and lastModifiedTime 
-            # is greater than 10 than send not modified message feel free to change this value.
-            #currently does not work for some unknown reason.
-            '''if currentTime - time > 10 :
-                connectionSocket.send(b'HTTP/1.0 304 Not Modified\r\n\r\n')'''
 
+            #If the difference between currentTime and lastModifiedTime 
+            # is greater than 600 seconds than send not modified message feel free to change this value.
+            #currently does not work for some unknown reason.
+            '''timeDiff = currentTime - time
+            if(timeDiff.total_seconds() > 600):
+                connectionSocket.send(b'HTTP/1.0 304 Not Modified\r\n\r\n')'''         
+            
             #open the file for reading
             file = open(filename)
-            bytes = file.read()
+            contents = file.read()
             file.close()
 
-            #Send one HTTP header line into socket
-            connectionSocket.send(b'HTTP/1.0 200 OK\r\n\r\n')
-            #Send the content of the requested file to the client
-            for i in range(0, len(bytes)):
-                connectionSocket.send(bytes[i].encode())
+            #Find duration of request
+            endTime = datetime.now()
+            timeout = endTime - currentTime
+
+            #Check if request took longer than 5 seconds. If so send 408 response.
+            if(timeout.total_seconds() > 5):
+                connectionSocket.send(b'HTTP/1.0 408 Request Timed Out\r\n\r\n')
+            else:
+                #Send one HTTP header line into socket
+                connectionSocket.send(b'HTTP/1.0 200 OK\r\n\r\n')
+
+                #Send the content of the requested file to the client
+                for i in range(0, len(contents)):
+                    connectionSocket.send(contents[i].encode())
     except:
         connectionSocket.send(b'HTTP/1.0 400 Bad request\r\n\r\n')
-    finally:
-        
+    finally: 
         # Close connection to client (but not welcoming socket)
         connectionSocket.close()
